65a6869c66d69aa903cb37fbb300c4fe39368d42
Dani Gutiérrez <a01068056@tec.mx>
2024-10-14 11:38:39 +0200

    refactor(base): use pylint

--- system_event_handler.py
+++ system_event_handler.py
@@ -1,8 +1,18 @@
+'''
+This module contains the SystemEventHandler class.
+'''
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
 
 class SystemEventHandler(FileSystemEventHandler):
-    def on_any_event(self, event: FileSystemEvent) -> None:
+    '''
+    This class is used to handle file system events.
+    It is a child class of FileSystemEventHandler
+    '''
+    def on_any_event(self, event: FileSystemEvent) -> FileSystemEvent:
+        '''
+        Event handler for any file system event
+        '''
         # Ignore __pycache__
         if '__pycache__' in event.src_path:
-            return  # Ignore this event if __pycache__ is in the path
+            return # Ignore this event if __pycache__ is in the path
         return event
