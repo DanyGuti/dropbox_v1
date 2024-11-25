df4fa487489d3be644b5642e264d940a7a3f20f6
963502 <963502@U025351.gi>
2024-11-25 19:09:04 +0100

    fix client

--- system_event_handler.py
+++ system_event_handler.py
@@ -1,7 +1,12 @@
 '''
 This module contains the SystemEventHandler class.
+
 '''
+
+import re
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
+
+swp_file_pattern = r"^.*\.swp$"
 
 class SystemEventHandler(FileSystemEventHandler):
     '''
@@ -13,6 +18,8 @@ class SystemEventHandler(FileSystemEventHandler):
         Event handler for any file system event
         '''
         # Ignore __pycache__
-        if '__pycache__' in event.src_path:
+        if '__pycache__' in event.src_path or re.match(swp_file_pattern, event.src_path):
+        
             return # Ignore this event if __pycache__ is in the path
+        print(event)
         return event
