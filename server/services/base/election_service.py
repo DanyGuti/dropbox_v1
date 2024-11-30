a8280663816b7fb38acabdad0d011206ee7a6cb1
Dani Gutiérrez <a01068056@tec.mx>
2024-11-30 21:09:17 +0100

    add interconnection nodes

--- server/services/base/election_service.py
+++ server/services/base/election_service.py
@@ -1,6 +1,7 @@
 '''
 File that contains the election service
 '''
+from rpyc.utils.classic import obtain
 from server.interfaces.election_interface import IElection
 from server.imports.import_server_base import rpyc
 
@@ -8,19 +9,19 @@ class Election(IElection):
     '''
     Election service of a new leader
     '''
+    nodes: list[tuple[str, int]] = []
     @rpyc.exposed
     def send_election_message(
         self,
         message_election: str,
-        nodes: list[tuple[str, int]],
-        curr_node: int
+        curr_node: int,
     ) -> tuple[list[str], int]:
         '''
         Send an election message to greater identified nodes
         '''
         # For every node greater than curr_node, send the election message
         accum_nodes: list[tuple[str, int]] = []
-        for ip, node in nodes:
+        for ip, node in self.nodes:
             if node > curr_node:
                 # Send the election message
                 accum_nodes.append((node, ip))
@@ -29,7 +30,11 @@ class Election(IElection):
         for ip, node in accum_nodes:
             # Send the election message
             try:
-                conn: rpyc.Connection = rpyc.connect(ip, 18861)
+                conn: rpyc.Connection = rpyc.connect(
+                    ip,
+                    18861,
+                    config={"allow_all_attrs": True, "allow_pickle": True}
+                )
                 response_slave: str = conn.root.receive_election_message(message_election)
                 responses.append(response_slave)
                 if response_slave == "ok":
@@ -63,6 +68,12 @@ class Election(IElection):
         '''
         if message_election == "election":
             return "ok"
-        elif message_election == "leader":
+        if message_election == "leader":
             return "ok"
         return "error"
+    @rpyc.exposed
+    def set_slaves_broadcasted(self, slaves: list[tuple[str, int]]) -> None:
+        '''
+        Receive the slaves broadcasted from the Coordinator
+        '''
+        self.nodes: list[tuple[str, int]] = obtain(slaves)
