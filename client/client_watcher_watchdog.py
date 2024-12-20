640306c33ea2053716ca59a50c3f7ebf810116da
Dani Gutiérrez <a01068056@tec.mx>
2024-12-20 22:16:38 +0100

    fix to client side

--- client/client_watcher_watchdog.py
+++ client/client_watcher_watchdog.py
@@ -38,15 +38,47 @@ class ClientWatcher(Client, SystemEventHandler):
         self.accumulation_timeout: float = 0.25
         self.lock: threading.Lock = threading.Lock()
         # self.root_path_stack: list = re.split(r'(\/)', CWD)
-    # Call the parent class
-    def on_any_event(self, event: FileSystemEvent) -> None:
+    def on_modified(self, event: FileModifiedEvent):
         '''
         Event handler for any file system event
         '''
-        super().on_any_event(event)
-        if not '__pycache__' in event.src_path:
+        if not isinstance(event, FileModifiedEvent):
+            return None
+        super().on_modified(event)
+        # if not '__pycache__' in event.src_path:
+        with self.lock:
+            self._dispatcher.append(event)
+    def on_created(self, event: FileCreatedEvent | DirCreatedEvent):
+        '''
+        Event handler for creating a file or directory
+        '''
+        event_handler: (FileCreatedEvent | DirCreatedEvent) = super().on_created(event)
+        if isinstance(event_handler, DirCreatedEvent):
+            # Handle a file creation
+            # if not '__pycache__' in event.src_path:
             with self.lock:
                 self._dispatcher.append(event)
+        elif isinstance(event_handler, FileCreatedEvent):
+            # Handle a dir creation
+            with self.lock:
+                self._dispatcher.append(event)
+    def on_deleted(self, event: FileDeletedEvent | DirDeletedEvent):
+        event_handler: (FileDeletedEvent | DirDeletedEvent) = super().on_deleted(event)
+        if isinstance(event_handler, DirDeletedEvent):
+            # Handle a file creation
+            # if not '__pycache__' in event.src_path:
+            with self.lock:
+                self._dispatcher.append(event)
+        elif isinstance(event_handler, FileDeletedEvent):
+            # Handle a dir creation
+            # if not '__pycache__' in event.src_path:
+            with self.lock:
+                self._dispatcher.append(event)
+    def on_moved(self, event):
+        super().on_moved(event)
+        # if not '__pycache__' in event.src_path:
+        with self.lock:
+            self._dispatcher.append(event)
     # Start watching with observer
     def start_watching(self) -> None:
         '''
@@ -92,7 +124,7 @@ class ClientWatcher(Client, SystemEventHandler):
         self.client.invocation_id += 1
         # Append the current request to the list of requests
         self.client.requests.append(self.client.request)
-        if action == 'file_created' or action == 'mv' or action == 'cp' or action == 'modified':
+        if action in ['file_created', 'mv', 'cp', 'modified']:
             if action == 'mv':
                 self.client.send_file_by_chunks(dst, file)
             else:
@@ -113,92 +145,48 @@ class ClientWatcher(Client, SystemEventHandler):
             while len(self._dispatcher) > 0:
                 curr_event: FileSystemEvent = self._dispatcher.pop(0)
                 accum_events.append(curr_event)
+            if any (re.match(swp_file_pattern, e.src_path) for e in accum_events):
+                accum_events = list(
+                    filter(
+                        lambda e : not re.match(swp_file_pattern, e.src_path),
+                        accum_events
+                    ))
             print(accum_events)
-            if(any (re.match(swp_file_pattern , e.src_path) for e in accum_events)):
-                accum_events = list(filter(lambda e : not re.match(swp_file_pattern , e.src_path), accum_events))
-                print(accum_events)
             while len(accum_events) > 0:
+                if all(isinstance(e, (DirModifiedEvent)) for e in accum_events):
+                    accum_events.clear()
+                    return
                 event: FileSystemEvent = accum_events[0]
-                if (len (accum_events) >= 4) and not isinstance(event, (FileMovedEvent))\
-                      or ((len (accum_events) == 6) and all(isinstance(e, 
-                        (FileCreatedEvent, DirModifiedEvent, FileModifiedEvent, FileClosedEvent,\
-                            DirModifiedEvent)) for e in accum_events)):
-                    file_event: FileSystemEvent = accum_events[1]
-                    file_name: str = self.construct_curr_path(file_event.src_path)
-                    accum_events.clear() # cp a file
-                    self.send_to_client(event.src_path, 'cp', file_name)
-                elif((len(accum_events) == 2) and isinstance(accum_events[0],DirModifiedEvent) and isinstance(accum_events[1],DirModifiedEvent)):
-                    print("Ha entrado")
+                if not any(isinstance(e, (FileMovedEvent, FileClosedEvent))\
+                    for e in accum_events):
                     accum_events.clear()
-                elif ((len (accum_events) == 3)
-                        and ((str(event.event_type) == 'modified')
-                            or (str(event.event_type) == 'created'))
-                        and (isinstance(event, FileCreatedEvent))):
+                    file_name : str = self.construct_curr_path(event.src_path)
+                if any((isinstance(e, (DirCreatedEvent)) for e in accum_events)):
+                    self.send_to_client(event.src_path, 'mkdir', file_name, "", True)
+                elif any((isinstance(e, (DirDeletedEvent)) for e in accum_events)):
+                    self.send_to_client(event.src_path, 'rmdir', file_name, "", True)
+                elif any((isinstance(e, (FileMovedEvent)) for e in accum_events)):
+                    moved_event = next((e for e in accum_events if isinstance(e
+                                    , FileMovedEvent
+                                )), None)
                     accum_events.clear()
-                    file_name: str = self.construct_curr_path(event.src_path)
-                    # Nano and created file SAVING IT
-                    self.send_to_client(event.src_path, 'file_created', file_name)
-                elif (isinstance(event, FileMovedEvent) or\
-                    (len(accum_events) >= 5 and all(isinstance(e, (FileMovedEvent, FileDeletedEvent, FileModifiedEvent, DirModifiedEvent)) for e in accum_events)) or
-                    (len(accum_events) == 3 and all(isinstance(e, (FileModifiedEvent, FileMovedEvent, DirModifiedEvent)) for e in accum_events))):
-                    if(all(isinstance(e, (DirModifiedEvent)) for e in accum_events)):
-                        accum_events.clear()
-                        return 
-                    moved_event = next((e for e in accum_events if isinstance(e, FileMovedEvent)), None)
-                    accum_events.clear() # mv a file
-                    # file_name: str = self.construct_curr_path(event.src_path)
                     file_dest : list = re.split(r'(\/)', moved_event.dest_path)
-                    print("mv a file")
-                    self.send_to_client(moved_event.src_path, 'mv', file_dest[-1], moved_event.dest_path)
-                elif (len(accum_events) >= 5 and all(isinstance(e, (FileModifiedEvent,  FileModifiedEvent, FileClosedEvent, DirModifiedEvent, DirModifiedEvent)) for e in accum_events)):
-                    accum_events.clear() # Modified a file
-                    file_name: str = self.construct_curr_path(event.src_path)
-                    self.send_to_client(event.src_path, 'modified', file_name)
-                elif ((len(accum_events) == 1) or (len(accum_events) == 2)
-                        and not (any(isinstance(e, (DirCreatedEvent)) for e in accum_events))
-                        and not (any(isinstance(e, (FileDeletedEvent)) for e in accum_events))
-                        and not (any(isinstance(e, (DirDeletedEvent)) for e in accum_events))
-                        or (len(accum_events) == 3) and not (any(isinstance(e, (DirCreatedEvent, FileDeletedEvent, DirDeletedEvent)) for e in accum_events))
-                        and (str(event.event_type) == 'modified')
-                        and (isinstance(event, FileModifiedEvent))):
-                    accum_events.clear() # Modified a file
-                    file_name: str = self.construct_curr_path(event.src_path)
-                    self.send_to_client(event.src_path, 'modified', file_name)
-                elif ((any(isinstance(e, (FileDeletedEvent)) for e in accum_events))):
-                    accum_events.clear() # Deleted a file
-                    file_name: str = self.construct_curr_path(event.src_path)
+                    self.send_to_client(
+                        moved_event.src_path,
+                        'mv',
+                        file_dest[-1],
+                        moved_event.dest_path
+                    )
+                elif any((isinstance(e, (FileDeletedEvent)) for e in accum_events)):
                     self.send_to_client(event.src_path, 'rm', file_name)
-                elif ((len(accum_events) == 2)
-                        and (str(event.event_type) == 'created')
-                        and not (event.is_directory)
-                        and (isinstance(event, FileCreatedEvent))):
-                    accum_events.clear()
-                    file_name: str = self.construct_curr_path(event.src_path)
+                elif (len(accum_events == 1) and isinstance(event, FileCreatedEvent)):
                     self.send_to_client(event.src_path, 'touch', file_name)
-                elif ((len(accum_events) == 2)
-                        and (event.is_directory)
-                        and (isinstance(event, DirCreatedEvent)
-                            or (isinstance(event, DirDeletedEvent)))):
-                    if event.event_type == 'created':
-                        accum_events.clear() # Created a directory
-                        file_name: str = self.construct_curr_path(event.src_path)
-                        self.send_to_client(event.src_path, 'mkdir', file_name, "", True)
-                    else:
-                        accum_events.clear() # Deleted a directory
-                        file_name: str = self.construct_curr_path(event.src_path)
-                        self.send_to_client(event.src_path, 'rmdir', file_name, "", True)
-                elif((len(accum_events) == 5)
-                        and (isinstance(event, FileCreatedEvent))):
-                        accum_events.clear()
-                        file_name: str = self.construct_curr_path(event.src_path)
-                        self.send_to_client(event.src_path, 'touch', file_name)
-                
-                elif((len(accum_events) == 6) and any(isinstance(e, (FileModifiedEvent)) for e in accum_events)):
-                    accum_events.clear() # Modified a file
-                    file_name: str = self.construct_curr_path(event.src_path)
-                    self.send_to_client(event.src_path, 'modified', file_name)
                 else:
-                    print(accum_events)
-                    accum_events.clear()
-                    print("No event to process recognized")
-                    continue
+                    if any((isinstance(e, (FileClosedEvent)) for e in accum_events)):
+                        file_event: FileSystemEvent = accum_events[1]
+                        file_name: str = self.construct_curr_path(file_event.src_path)
+                        accum_events.clear()
+                        self.send_to_client(event.src_path, 'cp', file_name)
+                    else:
+                        accum_events.clear()
+                        self.send_to_client(event.src_path, 'modified', file_name)
--- client/system_event_handler.py
+++ client/system_event_handler.py
@@ -1,18 +1,62 @@
 '''
 This module contains the SystemEventHandler class.
 '''
-from client.imports.import_base import FileSystemEvent, FileSystemEventHandler
+from client.imports.import_base import \
+    FileSystemEventHandler, FileMovedEvent, FileModifiedEvent, \
+        FileDeletedEvent, FileCreatedEvent, \
+        DirCreatedEvent, DirDeletedEvent
 
 class SystemEventHandler(FileSystemEventHandler):
     '''
     This class is used to handle file system events.
     It is a child class of FileSystemEventHandler
     '''
-    def on_any_event(self, event: FileSystemEvent) -> FileSystemEvent:
+    def __init__(self):
+        # self.last_directory = None
+        # self.last_file = None
+    # def on_any_event(self, event: FileSystemEvent) -> FileSystemEvent:
         '''
         Event handler for any file system event
         '''
         # Ignore __pycache__
-        if '__pycache__' in event.src_path:
-            return # Ignore this event if __pycache__ is in the path
+        # if '__pycache__' in event.src_path:
+#             return None # Ignore this event if __pycache__ is in the path
+        # return event
+    def on_modified(self, event: FileModifiedEvent):
+        '''
+        Handle the modification of a file
+        '''
+        # Ignore __pycache__
+        # if '__pycache__' in event.src_path:
+            # return # Ignore this event if __pycache__ is in the path
         return event
+    def on_created(self, event: FileCreatedEvent | DirCreatedEvent):
+        '''
+        Handle the creation of file or of directory
+        '''
+        # Ignore __pycache__
+        # if '__pycache__' in event.src_path:
+            # return None# Ignore this event if __pycache__ is in the path
+        if isinstance(event, FileCreatedEvent, DirCreatedEvent):
+            return event
+        return None
+    def on_deleted(self, event: FileDeletedEvent | DirDeletedEvent):
+        '''
+        Handle the deletion of a file or a directory
+        '''
+        if isinstance(event, FileDeletedEvent, DirDeletedEvent):
+            return event
+        return None
+        # Ignore __pycache__
+        # if '__pycache__' in event:
+            # return None# Ignore this event if __pycache__ is in the path
+    def on_moved(self, event: FileMovedEvent):
+        '''
+        Handle the moving of a file
+        '''
+        if isinstance(event, FileMovedEvent):
+            return event
+        return None
+        # Ignore __pycache__
+        # if '__pycache__' in event:
+            # return None# Ignore this event if __pycache__ is in the path
